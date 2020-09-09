2d4b96666974d6d02353b26d26eace17ff80eebe
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2020-09-09 13:25:18 -0500

    Add Jenkins Configurations
    Adding Global Jenkins configuration files used by the jenkins-cfg job to
    manage Global environment variables.
    
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,7 @@
+GIT_BASE=https://github.com/jquery/$PROJECT
+GIT_CLONE_URL=git@github.com:
+GIT_URL=https://github.com
+JENKINS_HOSTNAME=vex-yul-ojsf-jenkins-prod-1
+LOGS_SERVER=http://logs.openjsf.org
+S3_BUCKET=
+SILO=production
